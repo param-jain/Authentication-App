c70df4af716cbed91cb7e8dcc97f5c925348c3a3
Param Jain <param_jain@icloud.com>
2018-09-13 10:46:33 +0530

    Designing Login Page
    Created a Login Page.
    An Email and Password Input type Components by passing various props from LoginForm.
    Created a project on Firebase called as Authentication.
    Created a new Input Component from scratch.

--- Auth/src/components/LoginForm.js
+++ Auth/src/components/LoginForm.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import { Button, Card, CardSection } from './common'; 
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+    state = { email: '', password: '' };
+
     render() {
         return (
             <Card>
-                <CardSection />
-                
-                <CardSection />
+                <CardSection>
+                    <Input
+                        placeholder="user@domain.com"
+                        label="Email"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
+                    />
+                </CardSection>
+                    
+                <CardSection>
+                    <Input
+                        secureTextEntry={true}
+                        placeholder="password"
+                        label="Password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
 
                 <CardSection>
                     <Button>
